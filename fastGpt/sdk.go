90363c0b55255cc04b9f55bb131e5056d0b2d9bd
gangx <gangx6906@gmail.com>
2024-12-25 11:56:53 +0800

    更正路径

--- fastGpt/sdk.go
+++ fastGpt/sdk.go
@@ -45,7 +45,7 @@ func NewFastGptSdkClient(baseUrl string, apiKey string) *FastGptSdkClient {
 // CreateKnowledgeBase 创建知识库
 func (s *FastGptSdkClient) CreateKnowledgeBase(ReqInfo *CreateKnowledgeBaseReq) (*CreateKnowledgeBaseResp, error) {
 	var respInfo CreateKnowledgeBaseResp
-	err := s.httpCli.SendObjParse(http.MethodPost, s.baseUrl+"/core/dataset/create", &ReqInfo, &respInfo)
+	err := s.httpCli.SendObjParse(http.MethodPost, s.baseUrl+"core/dataset/create", &ReqInfo, &respInfo)
 	if err != nil {
 		return nil, err
 	}
@@ -54,7 +54,7 @@ func (s *FastGptSdkClient) CreateKnowledgeBase(ReqInfo *CreateKnowledgeBaseReq) 
 
 // GetKnowledgeList 获取知识库列表
 func (s *FastGptSdkClient) GetKnowledgeList(req *ReqListKnowledgeBase) (*ListKnowLedgeResp, error) {
-	resp, err := s.httpCli.SendParameter(http.MethodPost, s.baseUrl+"/core/dataset/list", req)
+	resp, err := s.httpCli.SendParameter(http.MethodPost, s.baseUrl+"core/dataset/list", req)
 	if err != nil {
 		return nil, err
 	}
@@ -69,7 +69,7 @@ func (s *FastGptSdkClient) GetKnowledgeList(req *ReqListKnowledgeBase) (*ListKno
 
 // GetDetailKnowledge 获取知识库详细信息
 func (s *FastGptSdkClient) GetDetailKnowledge(req *ReqDetailKnowledgeBase) (*DetailKnowledge, error) {
-	resp, err := s.httpCli.SendParameter(http.MethodGet, s.baseUrl+"/core/dataset/detail", req)
+	resp, err := s.httpCli.SendParameter(http.MethodGet, s.baseUrl+"core/dataset/detail", req)
 	if err != nil {
 		return nil, err
 	}
@@ -84,7 +84,7 @@ func (s *FastGptSdkClient) GetDetailKnowledge(req *ReqDetailKnowledgeBase) (*Det
 
 // UploadLocalFileSet 上传文件集合
 func (s *FastGptSdkClient) UploadLocalFileSet(req *UploadFileSetReq) (*UploadFileSetResp, error) {
-	resp, err := s.httpCli.SendForm(http.MethodPost, s.baseUrl+"/core/dataset/collection/create/localFile", req)
+	resp, err := s.httpCli.SendForm(http.MethodPost, s.baseUrl+"core/dataset/collection/create/localFile", req)
 	if err != nil {
 		return nil, err
 	}
@@ -100,28 +100,28 @@ func (s *FastGptSdkClient) UploadLocalFileSet(req *UploadFileSetReq) (*UploadFil
 // UploadLinkFileSet 添加链接集合
 func (s *FastGptSdkClient) UploadLinkFileSet(req *UpLoadLinkFileReq) (*UploadLinkFileResp, error) {
 	var resp UploadLinkFileResp
-	err := s.httpCli.SendObjParse(http.MethodPost, "/core/dataset/collection/create/link", req, &resp)
+	err := s.httpCli.SendObjParse(http.MethodPost, s.baseUrl+"core/dataset/collection/create/link", req, &resp)
 	return &resp, err
 }
 
 // UploadTextSet 添加文本集合
 func (s *FastGptSdkClient) UploadTextSet(req *UploadTextSetReq) (*UploadTextSetResp, error) {
 	var resp UploadTextSetResp
-	err := s.httpCli.SendObjParse(http.MethodPost, "/core/dataset/collection/create/text", req, &resp)
+	err := s.httpCli.SendObjParse(http.MethodPost, s.baseUrl+"core/dataset/collection/create/text", req, &resp)
 	return &resp, err
 }
 
 // GetAppChatHistory 获取应用程序的对话列表
 func (s *FastGptSdkClient) GetAppChatHistory(req *GetAppChatHistoryReq) (*GetAppChatHistoryResp, error) {
 	var resp GetAppChatHistoryResp
-	err := s.httpCli.SendObjParse(http.MethodGet, "/core/chat/getHistories", req, &resp)
+	err := s.httpCli.SendObjParse(http.MethodGet, s.baseUrl+"core/chat/getHistories", req, &resp)
 	return &resp, err
 }
 
 // GetAppChatHistoryRecords 获取应用程序聊天对话的记录列表
 func (s *FastGptSdkClient) GetAppChatHistoryRecords(req *GetChatRecordsReq) (*GetChatRecordsResp, error) {
 	var resp GetChatRecordsResp
-	err := s.httpCli.SendObjParse(http.MethodPost, "/core/chat/getPaginationRecords", req, &resp)
+	err := s.httpCli.SendObjParse(http.MethodPost, s.baseUrl+"core/chat/getPaginationRecords", req, &resp)
 	return &resp, err
 }
 
